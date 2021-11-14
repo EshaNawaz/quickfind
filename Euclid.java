a065c7917b4312ddfa7a0d7b36a1dadb74d607f0
EshaNawaz <eshanawaz17@gmail.com>
2021-11-14 22:48:37 +0500

    2nd update

--- Euclid.java
+++ Euclid.java
@@ -1,27 +1,27 @@
 public class Euclid{
     // recursive inplementation
-    public static int gcd(int p, int q){
-        if(q == 0) return p;
+    public static int gcd(int x, int y){
+        if(y == 0) return x;
         else 
         {
-            return gcd(q, p % q);
+            return gcd(y, x % y);
         }
     }
     // non-recursive implementation
-    public static int gcd2(int p, int q){
-    while(q != 0){
-        int temp = q;
-        q = p % q;
-        p = temp;
+    public static int gcd2(int x, int y){
+    while(y != 0){
+        int temp = y;
+        y = x % y;
+        x = temp;
     }
-    return p;
+    return x;
     }
     public static void main(String[] args){
-        int p = Integer.parseInt(args[0]);
-        int q = Integer.parseInt(args[1]);
-        int d  = gcd(p, q);
-        int d2 = gcd2(p,q);
-        System.out.println("gcd(" + p + ", " + q + ") = " + d);
-        System.out.println("gcd(" + p + ", " + q + ") = " + d2);
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int d  = gcd(x, y);
+        int d2 = gcd2(x,y);
+        System.out.println("gcd(" + x + ", " + y + ") = " + d);
+        System.out.println("gcd(" + x + ", " + y + ") = " + d2);
     }
 }
